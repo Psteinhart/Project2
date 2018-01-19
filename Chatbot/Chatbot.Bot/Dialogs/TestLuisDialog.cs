e7f00612a6518543612ef045031ac319eb9096f4
Phil Steinhart <Phillip.steinhart@gmail.com>
2018-01-19 11:32:06 -0500

    Update messages controller

--- Chatbot/Chatbot.Bot/Dialogs/TestLuisDialog.cs
+++ Chatbot/Chatbot.Bot/Dialogs/TestLuisDialog.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.FormFlow;
 using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
 
